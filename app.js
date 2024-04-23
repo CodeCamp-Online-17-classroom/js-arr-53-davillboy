5d690da3ef824e63bc33c3263b8c52a2dd2f1a36
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-24 01:25:56 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 // write code here
+let str = '31 45 12 67 34 86 23 37 19 41';
+let numbers = str.split(' ').map(Number);
+let sum = numbers.filter(num => num < 40).reduce((acc, curr) => acc + curr, 0);
+
+console.log("ผลรวมของเลขที่น้อยกว่า 40:", sum); 
